1942661e8cafdef5ebc675dcc1d186c4e1f8d2b4
Eric Tapia <erictapia1@gmail.com>
2021-01-02 23:27:29 -0800

    Changed the hello world to now be hello username.  The app route was changed as well.

--- flask_basics/flask_intro.py
+++ flask_basics/flask_intro.py
@@ -1,11 +1,17 @@
+import datetime
 from flask import Flask
+from markupsafe import escape
 
 
 app = Flask(__name__)
 
+@app.route('/hello/<username>')
+def hello_username(username):
+    return f'Hello { escape(username) }!'
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return f'{datetime.datetime.now()}'
 
 
 # How to run flask in dev environment
