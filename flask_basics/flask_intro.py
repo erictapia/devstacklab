f4f235ddcc9febf07f46bf7eb20a762f1e9e8beb
Eric Tapia <erictapia1@gmail.com>
2021-01-02 23:58:04 -0800

    Added each of the converter types.

--- flask_basics/flask_intro.py
+++ flask_basics/flask_intro.py
@@ -5,17 +5,30 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/hello/<username>')
+# Converter type: string - (default) accepts any text without a slash
+@app.route('/hello/<string:username>')
 def hello_username(username):
     return f'Hello { escape(username) }!'
 
+# Converter type: int - accepts positive integers
 @app.route('/echo/<int:post_id>')
-def show_post(post_id: int):
+def show_post(post_id):
     return f'Post {post_id}'
 
+# Converter type: int - accepts positive integers
+@app.route('/float/<float:fraction>')
+def show_float(fraction):
+    return f'Fraction: {fraction}'
+
+# Converter type: path - like string but also accepts slashes
 @app.route('/api/v0/<path:subpath>')
 def api_v0(subpath):
     return f'The subpath is: { escape(subpath) }'
+
+# Converter type: uuid - accepts UUID strings (32 hex)
+@app.route('/uuid/<uuid:uuid>')
+def show_uuid(uuid):
+    return f'UUID: {uuid}'
 
 @app.route('/')
 def index():
