76ad5cbf914c5c7932a3ba3a580cffc9a9063fb9
Eric Tapia <erictapia1@gmail.com>
2021-01-22 07:47:07 -0800

    Completed week 20, sets.

---
+++ m01b_intermediate/l_15_sets.py
@@ -0,0 +1,33 @@
+available_ips = set()
+used_ips = set()
+
+
+def print_ips():
+    print(f"Available IPs: {available_ips}")
+    print(f"Used IPs:      {used_ips}")
+
+for index in range(180, 200, 3):
+    available_ips.add(f"10.0.1.{index}")
+
+print_ips()
+
+while True:
+    ip_address = input("\nEnter IP addrss to allocate: ")
+
+    if not ip_address:
+        print("\nExiting 'sets' application")
+        exit()
+
+    if ip_address in available_ips:
+
+        print(f"-- allocated IP address: {ip_address}")
+        available_ips.remove(ip_address)
+        used_ips.add(ip_address)
+
+        print_ips()
+
+        if len(available_ips.intersection(used_ips)) > 0:
+            print("\nERROR! one or more IPs in both sets")
+
+    else:
+        print("-- IP address not found in available IPs\n")
