b399c2cf768f20709acb75ff7ed4b3730d64d200
Eric Tapia <erictapia1@gmail.com>
2021-01-27 10:41:55 -0800

    Completed the 'query parameters' tutorial.

--- main.py
+++ main.py
@@ -70,3 +70,69 @@ async def read_file(file_path: str):
     return{"file_path": file_path}
 
 
+# parameters that are not part of a path can be interpreted as "query" parameters
+# as long as it follows these rules:
+# - query parameters follow after a ?
+# - query parameters are separated by an & character
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/query")
+async def read_query(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+# Query parameters can be declare to be optional.
+# Using the typing.Optional class, the editor can help find errors in the code
+from typing import Optional
+
+@app.get("/optional_query/{item_id}")
+async def read_opt_query(item_id: str, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q} if q else {"item_id": item_id}
+
+# Declaring a query bool type
+@app.get("/bool_conversion/{item_id}")
+async def read_bool_query(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
+
+    if q:
+        item.update({"q": q})
+
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+
+    return item
+
+
+# Declaring multiple path parameters and query parameters
+# - declared order does not matter
+
+@app.get("/multiple/users/{user_id}/items/{item_id}")
+async def read_multiple_path(
+        # Path parameters
+        user_id: int, item_id: str,
+
+        # Query parameters
+        q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+
+    if q:
+        item.update({"q": q})
+
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+
+    return item
+
+
+# Declaring required query parameters
+# - add additional arguments to the definition
+# - do not declare any default
+@app.get("/required/items/{item_id}")
+async def read_required(item_id: str, needy: str):
+    return {"item_id": item_id, "needy": needy}
