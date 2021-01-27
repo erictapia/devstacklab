f30aa657b897c518fd720c5d7a4d2570739b84ac
Eric Tapia <erictapia1@gmail.com>
2021-01-26 21:53:17 -0800

    Added comments for future referencing.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 
-app = FastAPI()
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
 # AUTO OPENAPI DOCUMENTATION
 # localhost:8000/docs
 # localhost:8000/redoc
 # localhost:8000/openapi.json
+
+app = FastAPI()
+
+# FastAPI will handle requests with
+# - the path "/"
+# - using a "get" operation
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
