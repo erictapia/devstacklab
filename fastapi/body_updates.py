37a1463e4411d007814db3700fe2e735a9c8b1cb
Eric Tapia <erictapia1@gmail.com>
2021-02-10 21:34:45 -0800

    Completed "Body - Updates" of the FastAPI tutorial.

---
+++ fastapi/body_updates.py
@@ -0,0 +1,53 @@
+from typing import List, Optional
+
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
+    tax: float = 10.5
+    tags: List[str] = list()
+
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items[item_id]
+
+
+# Update with put
+# - update will replace any existing data with request data and any missing
+#   attribute will be replaced with the defaults
+# - any missing require attributes with not defaults will be set as null
+@app.put("/items/{item_id}", response_model=Item)
+async def update_item(item_id: str, item: Item):
+    update_item_encoded = jsonable_encoder(item)
+    items[item_id] = update_item_encoded
+    return update_item_encoded
+
+
+# Partial updates with PATCH
+# - use exclude_unset=True to generate a dict with only the data that was set
+# - use Model.copy to copy the partial changes
+# - use jsonable_encoder to convert to Python objects but JSON compatible
+@app.patch("/items/{item_id}", response_model=Item)
+async def update_item(item_id: str, item: Item):
+    stored_item_data = items[item_id]
+    stored_item_model = Item(**stored_item_data)
+    update_data = item.dict(exclude_unset=True)
+    updated_item = stored_item_model.copy(update=update_data)
+    items[item_id] = jsonable_encoder(updated_item)
+    return updated_item
